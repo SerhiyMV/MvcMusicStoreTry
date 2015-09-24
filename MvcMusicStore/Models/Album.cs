1b241ba4c245d303bf750aeb09c10d6a928bcf54
Serhii Maryn <serhiimaryn@outlook.com>
2015-09-24 17:28:37 +0300

    CRUD autp

---
+++ MvcMusicStore/Models/Album.cs
@@ -0,0 +1,15 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class Album
+    {
+        public virtual int AlbumID { get; set; }
+        public virtual int  GenredID { get; set; }
+        public virtual int ArtistID { get; set; }
+        public virtual string Title { get; set; }
+        public virtual decimal Price { get; set; }
+        public virtual string AlbumArtUrl { get; set; }
+        public virtual Genre Genre { get; set; }
+        public virtual Artist Artist { get; set; }
+
+    }
+}
---
+++ MvcMusicStore/Models/Artist.cs
@@ -0,0 +1,8 @@
+﻿namespace MvcMusicStore.Models
+{
+    public class Artist
+    {
+        public virtual int ArtistID { get; set; }
+        public virtual string Name { get; set; }
+    }
+}
